d638502a260f62b23de146b711f0348b2667ba16
codexmaker <codexmaker12@gmail.com>
2020-06-22 09:51:22 -0500

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { GooglePlaceDirective } from 'ngx-google-places-autocomplete';
+import { Address } from 'ngx-google-places-autocomplete/objects/address';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-maps';
+  @ViewChild("placesRef") placesRef : GooglePlaceDirective;
+  options = {
+    types : [],
+    componentRestrictions: { country: 'MX' }
+  }
+
+  title_add;
+  latitude;
+  longitude;
+  zoom;
+
+  ngOnInit() {
+    this.setCurrentLocation();
+  }
+  public handleAddressChange(address: Address) {
+    // Do some stuff
+    console.log(address);
+    console.log('Latitud : ' + address.geometry.location.lat());
+    console.log('Longitud : ' + address.geometry.location.lng());
+
+    this.latitude = address.geometry.location.lat();
+    this.longitude = address.geometry.location.lng();
+    
+  }
+  public setCurrentLocation() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+        this.zoom = 15;
+      })
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//A-material
+import { MatCardModule } from '@angular/material/card';
+//Flexbox
+import { FlexLayoutModule } from '@angular/flex-layout';
+//G-Mpas
+import { GooglePlaceModule } from "ngx-google-places-autocomplete";
+import { AgmCoreModule } from "@agm/core";
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    GooglePlaceModule,
+    AgmCoreModule.forRoot({
+      apiKey : 'your-api-key-goes-here',
+      libraries : ['places']
+    }),
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
